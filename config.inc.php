94ea42d9c5a578452976ead3f71de35ceaed7621
Jasmine Wang <jasminey.tea@gmail.com>
2019-12-22 17:25:21 -0600

    trying to add features

---
+++ config.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$i=0;
+$i++;
+$cfg['Servers'][$i]['user']          = 'jsmney';
+$cfg['Servers'][$i]['password']      = 'st0r3WRITER'; // use here your password
+$cfg['Servers'][$i]['auth_type']     = 'config';
+?>
